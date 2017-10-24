06806b4ecc5ebd6a227266b2d68ca70915f22c00
marcolilli <github@einmarco.com>
2017-10-24 12:34:28 +0200

    added subtraction

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ switch (operator) {
   case '+':
     result = operand1 + operand2;
     break;
+  case '-':
+    result = operand1 - operand2;
+    break;
   default:
     console.error('Not implemented:', operator);
 }
